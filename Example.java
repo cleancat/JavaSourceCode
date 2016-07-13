dae74fad5e5998d2605a2d3d114d6ed9a5a81308
liyang <15639954638@163.com>
2016-07-13 12:50:13 +0800

    添加Example.java

---
+++ Example.java
@@ -0,0 +1,25 @@
+import java.io.FileInputStream;
+import javax.swing.JOptionPane;
+class Example
+{
+	public static void main(String args[])
+	{
+      byte buffer[]=new byte[1024];
+	  String dir="D:/javatest/example.txt";
+	   int bytes;
+	  String str;
+	  try{
+		   FileInputStream fis=new FileInputStream(dir);
+		   bytes=fis.read(buffer);
+		   str=new String (buffer,0,bytes);
+	  }catch(Exception e)
+		  {
+            str=e.toString();
+	      }
+		  JOptionPane.showMessageDialog(null,str);
+		  System.exit(0);
+	  
+	 
+
+	}
+}
